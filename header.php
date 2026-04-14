6b5590cb48c429675e675e3c70ef64c89b92ca87
white-blanc <white-blanc@white-blancnoMacBook-Air.local>
2026-04-14 10:42:00 +0900

    first commit

---
+++ header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head prefix="og: http://ogp.me/ns#">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="format-detection" content="telephone=no">
+    <?php wp_head(); ?>
+</head>
+
+<body>
