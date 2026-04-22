b31b1b8722ce91ac3afaeb20c7abbb9fa1472e71
white-blanc <white-blanc@white-blancnoMacBook-Air.local>
2026-04-22 17:44:41 +0900

    セキュリティ設定

--- functions.php
+++ functions.php
@@ -4,3 +4,23 @@ if (! defined('ABSPATH')) exit;
 require_once dirname(__FILE__) . '/inc/index.php';
 
 add_theme_support('post-thumbnails');
+
+function my_filter_rest_endpoints($endpoints) {
+    if (isset($endpoints['/wp/v2/users'])) {
+        unset($endpoints['/wp/v2/users']);
+    }
+    if (isset($endpoints['/wp/v2/users/(?P<id>[\d]+)'])) {
+        unset($endpoints['/wp/v2/users/(?P<id>[\d]+)']);
+    }
+    return $endpoints;
+}
+add_filter('rest_endpoints', 'my_filter_rest_endpoints', 10, 1);
+
+add_filter('author_rewrite_rules', '__return_empty_array');
+function disable_author_archive() {
+    if (preg_match('#/author/.+#', $_SERVER['REQUEST_URI'])) {
+        wp_redirect(esc_url(home_url('/404.php')));
+        exit;
+    }
+}
+add_action('init', 'disable_author_archive');
